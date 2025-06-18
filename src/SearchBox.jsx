043c9593e4dc23fe2708a57c2e0cf892e153a0c4
KSingha01 <wdap100@gmail.com>
2025-06-19 02:11:03 +0530

    second commit

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,8 +6,8 @@ import './SearchBox.css';
 export default function SearchBox({updateInfo}){
     let [city, setCity] = useState('');
     let [error, setError] = useState(false);
-    const API_URL="https://api.openweathermap.org/data/2.5/weather" 
-    const API_KEY="dcdb5b5ba8af8ab94f5653aa86a9e920"
+    const API_URL=import.meta.env.VITE_API_URL
+    const API_KEY=import.meta.env.VITE_API_KEY
 
     let getWeatherInfo = async () => {
         try{
